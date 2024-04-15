02bbd945c0fed675df0218f0a736b88886bd0670
bans <bans@bansui-MacBookPro.local>
2024-04-15 21:26:05 +0900

    HyperRing MFA class, test sum function

---
+++ sdkCore/src/main/java/com/hyperring/sdk/core/HyperRingMFA.kt
@@ -0,0 +1,7 @@
+package com.hyperring.sdk.core
+
+public class HyperRingMFA {
+    fun sum(a: Int, b: Int) {
+        print("sum: ${a + b}");
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "HyperRingCore"
 include(":app")
+include(":sdkCore")
